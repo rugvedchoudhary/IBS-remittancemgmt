95eca782c24965e628604e9441243cf3520b13d3
sarashid <sarashid@DIN51002696.corp.capgemini.com>
2019-10-14 14:30:44 +0530

    Checking

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -1,4 +1,13 @@
 
 public class Array {
 
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 50;
+		int sum = a + b;
+		System.out.println("Sum is :" + sum);
+		System.out.println("Hello");
+	}
+
 }
+
